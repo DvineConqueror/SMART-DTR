a032f544f1fed79c2ac8c629f072f32e3ea0e72e
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2024-09-25 19:14:10 +0800

    added retrofit in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,11 @@ android {
 }
 
 dependencies {
+    
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
