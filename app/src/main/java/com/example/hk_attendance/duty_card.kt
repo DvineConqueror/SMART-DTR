9b62bc0e6daba76b90a019ae0cecde3cec6562dc
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2024-10-04 18:32:53 +0800

    duty cards and updated recyclerview

---
+++ app/src/main/java/com/example/hk_attendance/duty_card.kt
@@ -0,0 +1,20 @@
+package com.example.hk_attendance
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class duty_card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_duty_card)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
