9e01e6f5385bd1087a6b9aa576ead338e1dfb48e
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2024-10-04 11:56:07 +0800

    revision

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
 
 
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/example/hk_attendance/home.kt
+++ app/src/main/java/com/example/hk_attendance/home.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [appointment.newInstance] factory method to
+ * Use the [home.newInstance] factory method to
  * create an instance of this fragment.
  */
-class appointment : Fragment() {
+class home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class appointment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appointment, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class appointment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment appointment.
+         * @return A new instance of fragment home.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            appointment().apply {
+            home().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
