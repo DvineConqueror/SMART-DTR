48bd517982b9b127e9a27113124c26dad004793f
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2024-10-04 13:25:36 +0800

    changed the make appointments to fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-
-
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
